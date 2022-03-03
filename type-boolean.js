49117d992b6336c95c49e09b57e5ecc7cd30c3fb
wendellsantos2 <wemdellcos@gmail.com>
2022-03-03 10:47:56 -0300

    Exemplos da aula 01

---
+++ type-boolean.js
@@ -0,0 +1,8 @@
+
+const primeiroNumero = 5;
+const segundoNumero = 5; 
+let cadastroAtivado = true; 
+
+const texto1 = "Alura ";
+const texto2 = "a";
+console.log(texto1 === texto2)
---
+++ type-string.js
@@ -0,0 +1,8 @@
+const texto1 = "olá mundo";
+const texto2 = 'Olá mundo';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros; "123";
+const citacao = "meu nome é ";
+const meunome="Wendell";
+
+console.log(citacao + meunome)
