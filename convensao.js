75e80186262e1896ef98265ded53000a225947d5
wendellsantos2 <wemdellcos@gmail.com>
2022-03-03 14:40:06 -0300

    codigos boolean e conversao

---
+++ convensao.js
@@ -0,0 +1,13 @@
+//tipos de dados 
+//booleanos 
+
+
+//conversão implicita 
+
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero + numeroString)
+
+
+//conversão explicita 
---
+++ true-false.js
@@ -0,0 +1,18 @@
+const usuarioLogado =    true;
+
+const contapaga = false;
+ 
+ //0 => false 
+ //1 => true
+
+
+ 
+//null valor vazio 
+
+let minhaVar ;
+let varNull = null ; 
+
+let numero = 3; 
+let text = "Alura ";
+console.log(typeof minhaVar)
+console.log(typeof varNull) 
